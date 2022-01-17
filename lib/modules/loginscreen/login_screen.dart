8325bd4c821b0ca722e42ee61ef1e3b6365a4419
aliaa3atef <aliaaatef440@gmail.com>
2022-01-17 02:27:58 +0200

    design1

--- lib/modules/loginscreen/login_screen.dart
+++ lib/modules/loginscreen/login_screen.dart
@@ -8,6 +8,7 @@ import 'package:Hayah_Clinic/shared/widgets/DefualtTextFormField.dart';
 import 'package:Hayah_Clinic/shared/widgets/GoogleSignInButton.dart';
 import 'package:Hayah_Clinic/shared/widgets/TitleText.dart';
 import 'package:conditional_builder/conditional_builder.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -25,118 +26,136 @@ class LoginScreen extends StatelessWidget {
         builder: (context, state) {
           var cubit = LoginCubit.get(context);
           return Scaffold(
-            appBar: AppBar(),
-            body: Center(
-              child: Form(
-                key: keysss,
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TitleText("login"),
-                        const SizedBox(
-                          height: 30.0,
-                        ),
-                        DefaultTextFormField(
-                          hint: 'Email Address',
-                          validate: (value) {
-                            if (value.isEmpty) {
-                              return 'Email Address Must Not Be Empty';
-                            }
-                            return null;
-                          },
-                          prefixicon: Icon(Icons.email),
-                          controller: email,
-                          type: TextInputType.emailAddress,
-                        ),
-                        const SizedBox(
-                          height: 15.0,
-                        ),
-                        DefaultTextFormField(
-                          suffixicon: cubit.isPassword
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          sufOnPressed: () {
-                            cubit.setVis();
-                          },
-                          onSubmitted: (value) {
-                            if (keysss.currentState.validate())
-                              cubit.userLogin(
-                                  context: context,
-                                  email: email.text,
-                                  password: pass.text);
-                          },
-                          isPassword: cubit.isPassword,
-                          prefixicon: Icon(Icons.lock),
-                          controller: pass,
-                          type: TextInputType.visiblePassword,
-                          hint: 'Password',
-                          validate: (value) {
-                            if (value.isEmpty) {
-                              return 'Password Must Not Be Empty';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 40.0,
-                        ),
-                        ConditionalBuilder(
-                          condition: state is! LoginLoadingState,
-                          builder: (context) => Container(
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(18.0)),
-                            child: DefaultButton(
-                                fun: () {
-                                  if (keysss.currentState.validate())
-                                    cubit.userLogin(
-                                        context: context,
-                                        email: email.text,
-                                        password: pass.text);
-                                },
-                                child: Text(
-                                  'LOGIN',
-                                  style: Theme.of(context).textTheme.bodyText2,
-                                )),
-                          ),
-                          fallback: (context) => Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        GoogleSignInButton(),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Don\'t Have an Account?',
-                              style: Theme.of(context).textTheme.subtitle1,
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: keysss,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        width: double.infinity,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
                             ),
-                            TextButton(
-                                onPressed: () {
-                                  NavigateTo(context, RegisterScreen());
-                                },
-                                child: Text(
-                                  'Register Now',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subtitle1
-                                      .copyWith(
-                                        color: defColor,
-                                      ),
-                                ))
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/cover.jpg"),
+                              fit: BoxFit.fill,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TitleText("login"),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            DefaultTextFormField(
+                              hint: 'Email Address',
+                              validate: (value) {
+                                if (value.isEmpty) {
+                                  return 'Email Address Must Not Be Empty';
+                                }
+                                return null;
+                              },
+                              prefixicon: Icon(Icons.email),
+                              controller: email,
+                              type: TextInputType.emailAddress,
+                            ),
+                            const SizedBox(
+                              height: 15.0,
+                            ),
+                            DefaultTextFormField(
+                              suffixicon: cubit.isPassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              sufOnPressed: () {
+                                cubit.setVis();
+                              },
+                              onSubmitted: (value) {
+                                if (keysss.currentState.validate())
+                                  cubit.userLogin(
+                                      context: context,
+                                      email: email.text,
+                                      password: pass.text);
+                              },
+                              isPassword: cubit.isPassword,
+                              prefixicon: Icon(Icons.lock),
+                              controller: pass,
+                              type: TextInputType.visiblePassword,
+                              hint: 'Password',
+                              validate: (value) {
+                                if (value.isEmpty) {
+                                  return 'Password Must Not Be Empty';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            ConditionalBuilder(
+                              condition: state is! LoginLoadingState,
+                              builder: (context) => Container(
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18.0)),
+                                child: DefaultButton(
+                                    fun: () {
+                                      if (keysss.currentState.validate())
+                                        cubit.userLogin(
+                                            context: context,
+                                            email: email.text,
+                                            password: pass.text);
+                                    },
+                                    child: Text(
+                                      'LOGIN',
+                                      style: Theme.of(context).textTheme.bodyText2,
+                                    )),
+                              ),
+                              fallback: (context) => Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            GoogleSignInButton(),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Don\'t Have an Account?',
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                ),
+                                TextButton(
+                                    onPressed: () {
+                                      NavigateTo(context, RegisterScreen());
+                                    },
+                                    child: Text(
+                                      'Register Now',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .subtitle1
+                                          .copyWith(
+                                            color: defColor,
+                                          ),
+                                    ))
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
